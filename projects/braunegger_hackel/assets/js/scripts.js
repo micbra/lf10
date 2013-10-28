6ad871ce0f0f0da186af730c2ff5f64de80f4232
Michael Braunegger <michael.braunegger@gmail.com>
2013-10-28 04:57:37 +0100

    finished line chart

--- projects/braunegger_hackel/assets/js/scripts.js
+++ projects/braunegger_hackel/assets/js/scripts.js
@@ -39,15 +39,15 @@ window.onload = function () {
 
 				bars.add(bar2007, bar2008, bar2009, bar2010, bar2011, bar2012);
 
-				line = drawBarChart.line(67, 15, 399, 15).stroke({ width: 1, color: '#fff', opacity: 0.25 }),
-				line = drawBarChart.line(67, 72, 399, 72).stroke({ width: 1, color: '#fff', opacity: 0.25 }),
-				line = drawBarChart.line(67, 129, 399, 129).stroke({ width: 1, color: '#fff', opacity: 0.25 }),
-				line = drawBarChart.line(67, 185, 399, 185).stroke({ width: 1, color: '#fff', opacity: 0.25 }),
-				line = drawBarChart.line(67, 242, 399, 242).stroke({ width: 1, color: '#fff', opacity: 0.25 }),
-				line = drawBarChart.line(67, 299, 399, 299).stroke({ width: 1, color: '#fff', opacity: 0.25 }),
-				line = drawBarChart.line(67, 356, 399, 356).stroke({ width: 1, color: '#fff', opacity: 0.25 }),
-				line = drawBarChart.line(67, 412, 399, 412).stroke({ width: 1, color: '#fff', opacity: 0.25 }),
-				line = drawBarChart.line(67, 469, 399, 469).stroke({ width: 1, color: '#fff', opacity: 0.25 });
+				drawBarChart.line(67, 15, 399, 15).stroke({ width: 1, color: '#fff', opacity: 0.25 });
+				drawBarChart.line(67, 72, 399, 72).stroke({ width: 1, color: '#fff', opacity: 0.25 });
+				drawBarChart.line(67, 129, 399, 129).stroke({ width: 1, color: '#fff', opacity: 0.25 });
+				drawBarChart.line(67, 185, 399, 185).stroke({ width: 1, color: '#fff', opacity: 0.25 });
+				drawBarChart.line(67, 242, 399, 242).stroke({ width: 1, color: '#fff', opacity: 0.25 });
+				drawBarChart.line(67, 299, 399, 299).stroke({ width: 1, color: '#fff', opacity: 0.25 });
+				drawBarChart.line(67, 356, 399, 356).stroke({ width: 1, color: '#fff', opacity: 0.25 });
+				drawBarChart.line(67, 412, 399, 412).stroke({ width: 1, color: '#fff', opacity: 0.25 });
+				drawBarChart.line(67, 469, 399, 469).stroke({ width: 1, color: '#fff', opacity: 0.25 });
 
 				bars.each(function(){
 					this.filter(function(add) {
@@ -84,9 +84,6 @@ window.onload = function () {
 			rim = drawPieChart.path('m152,151.99699l26.047,-147.71899c9.79001,1.727 18.42601,4.123 27.707,7.684l-53.754,140.03499z').fill('#C96691').stroke({ width: 2, color: '#fff' }).attr('class', 'pie').attr('id', 'rim'),
 			win = drawPieChart.path('m152,151.99699l0,-149.99699c9.94099,0 16.257,0.553 26.04601,2.278l-26.04601,147.71899z').fill('#FFDD00').stroke({ width: 2, color: '#fff' }).attr('class', 'pie').attr('id', 'win');
 
-			pie = drawPieChart.set();
-			pie.add(android, ios, rim, win);
-
 			var pieChartData = results.data.pieChart;
 
 			var pieElements = $('#pieChartContainer .pie').each(function() {
@@ -106,6 +103,76 @@ window.onload = function () {
 						$('.orange .tooltip span').stop().animate({"opacity": 0});
 					});
 				});
+
+			//Line Chart
+			var drawLineChart = SVG('lineChartContainer').size('100%', '100%');
+
+			var line_android = drawLineChart.polyline('362.238,84.29 274.901,234.296 187.563,335.549 100.226,378.3').fill('none').stroke({ width: 3, color: '#009BB1' }).attr('class', 'line').attr('id', 'android'),
+				line_ios = drawLineChart.polyline('362.238,46.791 274.901,168.669 187.563,253.046 100.226,339.299').fill('none').stroke({ width: 3, color: '#EA5437' }).attr('class', 'line').attr('id', 'ios'),
+				line_rim = drawLineChart.polyline('362.238,346.799 274.901,353.174 187.563,382.05').fill('none').stroke({ width: 3, color: '#C96691' }).attr('class', 'line').attr('id', 'rim'),
+				line_win = drawLineChart.polyline('362.238,339.299 274.901,358.049 187.563,377.55').fill('none').stroke({ width: 3, color: '#FFDD00' }).attr('class', 'line').attr('id', 'win'),
+
+				i1 = drawLineChart.circle('8').fill('#fff').stroke({width: 2, color: '#E3000B'}).attr('cx', '100').attr('cy', '339').attr('class', 'point').attr('id', 'i1'),
+				i2 = drawLineChart.circle('8').fill('#fff').stroke({width: 2, color: '#E3000B'}).attr('cx', '188').attr('cy', '253').attr('class', 'point').attr('id', 'i2'),
+				i3 = drawLineChart.circle('8').fill('#fff').stroke({width: 2, color: '#E3000B'}).attr('cx', '275').attr('cy', '169').attr('class', 'point').attr('id', 'i3'),
+				i4 = drawLineChart.circle('8').fill('#fff').stroke({width: 2, color: '#E3000B'}).attr('cx', '362').attr('cy', '47').attr('class', 'point').attr('id', 'i4'),
+
+				a1 = drawLineChart.circle('8').fill('#fff').stroke({width: 2, color: '#009BB1'}).attr('cx', '100').attr('cy', '378').attr('class', 'point').attr('id', 'a1'),
+				a2 = drawLineChart.circle('8').fill('#fff').stroke({width: 2, color: '#009BB1'}).attr('cx', '188').attr('cy', '336').attr('class', 'point').attr('id', 'a2'),
+				a3 = drawLineChart.circle('8').fill('#fff').stroke({width: 2, color: '#009BB1'}).attr('cx', '275').attr('cy', '234').attr('class', 'point').attr('id', 'a3'),
+				a4 = drawLineChart.circle('8').fill('#fff').stroke({width: 2, color: '#009BB1'}).attr('cx', '362').attr('cy', '84').attr('class', 'point').attr('id', 'a4'),
+
+				w1 = drawLineChart.circle('8').fill('#fff').stroke({width: 2, color: '#FFDD00'}).attr('cx', '188').attr('cy', '378').attr('class', 'point').attr('id', 'w1'),
+				w2 = drawLineChart.circle('8').fill('#fff').stroke({width: 2, color: '#FFDD00'}).attr('cx', '275').attr('cy', '358').attr('class', 'point').attr('id', 'w2'),
+				w3 = drawLineChart.circle('8').fill('#fff').stroke({width: 2, color: '#FFDD00'}).attr('cx', '362').attr('cy', '339').attr('class', 'point').attr('id', 'w3'),
+
+				b1 = drawLineChart.circle('8').fill('#fff').stroke({width: 2, color: '#C96691'}).attr('cx', '188').attr('cy', '378').attr('class', 'point').attr('id', 'b1'),
+				b2 = drawLineChart.circle('8').fill('#fff').stroke({width: 2, color: '#C96691'}).attr('cx', '275').attr('cy', '353').attr('class', 'point').attr('id', 'b2'),
+				b3 = drawLineChart.circle('8').fill('#fff').stroke({width: 2, color: '#C96691'}).attr('cx', '362').attr('cy', '347').attr('class', 'point').attr('id', 'b3');
+
+
+			drawLineChart.line(83, 9, 379, 9).stroke({ width: 1, color: '#fff', opacity: 0.25 });
+			drawLineChart.line(83, 28, 379, 28).stroke({ width: 1, color: '#fff', opacity: 0.25 });
+			drawLineChart.line(83, 46, 379, 46).stroke({ width: 1, color: '#fff', opacity: 0.25 });
+			drawLineChart.line(83, 65, 379, 65).stroke({ width: 1, color: '#fff', opacity: 0.25 });
+			drawLineChart.line(83, 84, 379, 84).stroke({ width: 1, color: '#fff', opacity: 0.25 });
+			drawLineChart.line(83, 103, 379, 103).stroke({ width: 1, color: '#fff', opacity: 0.25 });
+			drawLineChart.line(83, 121, 379, 121).stroke({ width: 1, color: '#fff', opacity: 0.25 });
+			drawLineChart.line(83, 140, 379, 140).stroke({ width: 1, color: '#fff', opacity: 0.25 });
+			drawLineChart.line(83, 159, 379, 159).stroke({ width: 1, color: '#fff', opacity: 0.25 });
+			drawLineChart.line(83, 178, 379, 178).stroke({ width: 1, color: '#fff', opacity: 0.25 });
+			drawLineChart.line(83, 196, 379, 196).stroke({ width: 1, color: '#fff', opacity: 0.25 });
+			drawLineChart.line(83, 215, 379, 215).stroke({ width: 1, color: '#fff', opacity: 0.25 });
+			drawLineChart.line(83, 234, 379, 234).stroke({ width: 1, color: '#fff', opacity: 0.25 });
+			drawLineChart.line(83, 253, 379, 253).stroke({ width: 1, color: '#fff', opacity: 0.25 });
+			drawLineChart.line(83, 271, 379, 271).stroke({ width: 1, color: '#fff', opacity: 0.25 });
+			drawLineChart.line(83, 290, 379, 290).stroke({ width: 1, color: '#fff', opacity: 0.25 });
+			drawLineChart.line(83, 309, 379, 309).stroke({ width: 1, color: '#fff', opacity: 0.25 });
+			drawLineChart.line(83, 328, 379, 328).stroke({ width: 1, color: '#fff', opacity: 0.25 });
+			drawLineChart.line(83, 346, 379, 346).stroke({ width: 1, color: '#fff', opacity: 0.25 });
+			drawLineChart.line(83, 365, 379, 365).stroke({ width: 1, color: '#fff', opacity: 0.25 });
+			drawLineChart.line(83, 384, 379, 384).stroke({ width: 1, color: '#fff', opacity: 0.25 });
+
+			var lineChartData = results.data.lineChart;
+
+			var pointElements = $('#lineChartContainer .point').each(function() {
+					this.instance.on('mouseover', function(){
+						var color = this.instance.attr('stroke');
+						$(this).css('stroke', shadeColor(color, 10));
+						var id = this.id;
+							$('.turquoise .tooltip span').text(lineChartData[id].val).stop().animate({"opacity": 0.85});
+							$('.turquoise .tooltip').css({
+								'left': this.instance.attr('cx')+45 + 'px',
+								'top': this.instance.attr('cy')+80 + 'px'
+							});
+						
+					});
+					this.instance.on('mouseout', function(){
+						$(this).css('stroke', this.instance.attr('stroke'));
+						$('.turquoise .tooltip span').stop().animate({"opacity": 0});
+					});
+				});
+
 
 
 				
